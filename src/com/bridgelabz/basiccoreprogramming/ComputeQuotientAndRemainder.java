d1fdb08b893f08188e77a4bae60d0d73f8e21f4c
akashp69 <108648856+akashp69@users.noreply.github.com>
2022-11-28 02:33:23 +0530

    Update ComputeQuotientAndRemainder.java

--- src/com/bridgelabz/basiccoreprogramming/ComputeQuotientAndRemainder.java
+++ src/com/bridgelabz/basiccoreprogramming/ComputeQuotientAndRemainder.java
@@ -3,8 +3,14 @@ package com.bridgelabz.basiccoreprogramming;
 import java.util.Scanner;
 
 public class ComputeQuotientAndRemainder {
+     /**
+    *This  Method is Created For Find The Value of quotient and remainder
+    */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+    /**
+    *Enter the Values of Dividend and Divisors For getting the values of  quotient and remainder
+    */
         int dividend;
         int divisor;
         int quotient;
@@ -15,6 +21,9 @@ public class ComputeQuotientAndRemainder {
         divisor = scanner.nextInt();
         quotient = dividend / divisor;
         remainder = dividend % divisor;
+    /**
+    *Print the values of quotient and remainder
+    */
         System.out.println("quotient = "+quotient + ";" + "remainder = "+remainder);
     }
 }
