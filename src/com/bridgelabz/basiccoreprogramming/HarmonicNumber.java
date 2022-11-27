75091dfbc91170157c3f137ac2631814c1bc2b6c
akashp69 <108648856+akashp69@users.noreply.github.com>
2022-11-28 02:38:38 +0530

    Update HarmonicNumber.java

--- src/com/bridgelabz/basiccoreprogramming/HarmonicNumber.java
+++ src/com/bridgelabz/basiccoreprogramming/HarmonicNumber.java
@@ -3,12 +3,18 @@ package com.bridgelabz.basiccoreprogramming;
 import java.util.Scanner;
 
 public class HarmonicNumber {
+    /**
+    *This  Method is Created For Find The  Harmonic Number
+    */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number;
         double result=0.0;
         System.out.println("Enter the Nth Harmonic Number");
         number = scanner.nextInt();
+    /**
+    *used While Loop For getting Harmonic Number
+    */
         while (number>0) {
             result =result+ (double)1/number;
             number--;
