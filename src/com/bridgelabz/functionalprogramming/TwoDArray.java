72ee50645981486542c72a27fbab2745307ccae0
akashp69 <108648856+akashp69@users.noreply.github.com>
2022-11-28 02:46:35 +0530

    Update TwoDArray.java

--- src/com/bridgelabz/functionalprogramming/TwoDArray.java
+++ src/com/bridgelabz/functionalprogramming/TwoDArray.java
@@ -3,19 +3,27 @@ package com.bridgelabz.functionalprogramming;
 import java.util.Scanner;
 
 public class TwoDArray {
+    /**
+    *This  Method is Created For Print the 2D Array
+    */
     public static void main(String[] args) {
+        
 
         int row, col, i, j;
         int[][] arr = new int[3][3];
         Scanner scanner = new Scanner(System.in);
 
-        // enter row and column for array.
+    /**
+    *Enter Rows And columns 
+    */
         System.out.print("Enter row for the array (max 3) : ");
         row = scanner.nextInt();
         System.out.print("Enter column for the array (max 3) : ");
         col = scanner.nextInt();
 
-        // enter array elements.
+    /**
+    *Enter Elements of array for rows And colums
+    */
         System.out.println("Enter " + (row * col) + " Array Elements : ");
         for (i = 0; i < row; i++) {
             for (j = 0; j < col; j++) {
@@ -23,7 +31,9 @@ public class TwoDArray {
             }
         }
 
-        // the 2D array is here.
+    /**
+    *Using For Loop For Print The 2D Array
+    */
         System.out.print("The Array is :");
         for (i = 0; i < row; i++) {
             for (j = 0; j < col; j++) {
