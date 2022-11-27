c8c673449c7e98e9ebb023af1c10be2563cc71d5
akashp69 <108648856+akashp69@users.noreply.github.com>
2022-11-28 02:35:53 +0530

    Update FactorOfNumber.java

--- src/com/bridgelabz/basiccoreprogramming/FactorOfNumber.java
+++ src/com/bridgelabz/basiccoreprogramming/FactorOfNumber.java
@@ -3,12 +3,21 @@ package com.bridgelabz.basiccoreprogramming;
 import java.util.Scanner;
 
 public class FactorOfNumber {
+     /**
+    *This  Method is Created For Find The  Factors of Numbers
+    */
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+    /**
+    *Enter a number for getting th factors of that number 
+    */
         System.out.println("Enter number");
         int number = scanner.nextInt();
         System.out.println("Prime factors for the given numbers:");
+    /**
+    *Used For loop for get factors 
+    */
 
         for (int integer = 2; integer <= number; integer++) {
 
