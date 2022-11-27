3f50f21d685eef416f49f5bb86b5aa87e6d1a780
akashp69 <108648856+akashp69@users.noreply.github.com>
2022-11-28 02:26:48 +0530

    Update CheckAlphabetsVowelsOrConsonant.java

--- src/com/bridgelabz/basiccoreprogramming/CheckAlphabetsVowelsOrConsonant.java
+++ src/com/bridgelabz/basiccoreprogramming/CheckAlphabetsVowelsOrConsonant.java
@@ -3,15 +3,24 @@ package com.bridgelabz.basiccoreprogramming;
 import java.util.Scanner;
 
 public class CheckAlphabetsVowelsOrConsonant {
+    /**
+    *This Static Method is Created For checking alphabets vowel or not
+    */
     static void checkAlphabet(){
         char alphabet;
         Scanner scanner = new Scanner(System.in);
         char ch = scanner.next().charAt(0);
+    /**
+    *Using if-else for comparing given alphabets is vowel or not
+    */
         if(ch=='a' || ch=='e' || ch=='i' || ch=='o'|| ch=='u')
             System.out.println("Vowel");
         else
             System.out.println("NOT Vowel");
     }
+     /**
+    *This  Method is Created For call the static method checkAlphabet
+    */
     public static void main(String[] args) {
 
         System.out.println("Check Letter is Vowel Or NOT Enter Any Alphabet");
