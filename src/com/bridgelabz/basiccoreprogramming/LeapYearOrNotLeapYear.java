57f50acf4c1e56dc24654c9b5a6f53f460c60ede
akashp69 <108648856+akashp69@users.noreply.github.com>
2022-11-28 02:42:26 +0530

    Update LeapYearOrNotLeapYear.java

--- src/com/bridgelabz/basiccoreprogramming/LeapYearOrNotLeapYear.java
+++ src/com/bridgelabz/basiccoreprogramming/LeapYearOrNotLeapYear.java
@@ -3,15 +3,24 @@ package com.bridgelabz.basiccoreprogramming;
 import java.util.Scanner;
 
 public class LeapYearOrNotLeapYear {
+    /**
+    *This static Method is Created For Find year is Leap Year or not
+    */
     static void checkLeapYear(){
         int year;
         Scanner scanner = new Scanner(System.in);
         year = scanner.nextInt();
+     /**
+    *Using if-else to known the given year is leap year or not
+    */
         if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
             System.out.println("Leap Year");
         else
             System.out.println("Not A Leap Year");
     }
+    /**
+    *This Main Method is Created For call the static method checkLeapYear
+    */
 
     public static void main(String[] args) {
         System.out.println("Enter Year For Checking Given Year is Leap Year Or NOT Leap Year");
